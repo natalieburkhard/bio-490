6b9db5e0002af2542bff7f7a028b027c7c7873e8
natalieburkhard <97681247+natalieburkhard@users.noreply.github.com>
2022-01-18 15:48:34 -0500

    Add files via upload
    first commit to upload file to repository

---
+++ R_basic.R
@@ -0,0 +1,18 @@
+# I am initiating a variable 'x' and assigning a value to it using <-
+
+x <- 1/40
+# print the value of x
+x
+# working with the variable 
+log(x)
+
+# changing the value of x
+x <- 100
+x <- x + 1
+y <- x * 2
+
+# can also use equal sign for assignment
+x = 1/40
+
+
+1:5
